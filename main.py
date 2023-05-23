9985ab209bfba65f2dec2820163f12ae526d1b67
daviot69 <ianjcampbell69@gmail.com>
2023-05-23 22:46:58 +0100

    Exporting all team results to csv

--- main.py
+++ main.py
@@ -1,23 +1,46 @@
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
+import time
+
+years = range(2023, 2022, -1)
 
 standings_url = "https://fbref.com/en/comps/9/Premier-League-Stats"
+base_url = "https://fbref.com"
 
 if __name__ == "__main__":
-    data = requests.get(standings_url)
 
-    soup = BeautifulSoup(data.text, features="html.parser")
+    all_matches = []
 
-    standings_table = soup.select('table.stats_table')[0]
-    links = standings_table.find_all('a')
-    links = [lnk.get("href") for lnk in links]
-    links = [lnk for lnk in links if '/squads' in lnk]
+    for year in years:
 
-    team_urls = [f"https://fbref.com{lnk}" for lnk in links]
+        print(f"Processing {year}")
+        data = requests.get(standings_url)
 
-    team_url = team_urls[6]
-    data = requests.get(team_url)
+        soup = BeautifulSoup(data.text, features="html.parser")
 
-    matches = pd.read_html(data.text, match="Scores & Fixtures")
-    print(matches)
+        standings_table = soup.select('table.stats_table')[0]
+        links = standings_table.find_all('a')
+        links = [lnk.get("href") for lnk in links]
+        links = [lnk for lnk in links if '/squads' in lnk]
+
+        team_urls = [f"{base_url}{lnk}" for lnk in links]
+
+        for team_url in team_urls:
+            team_name = team_url.split("/")[-1].replace("-Stats", "").replace("-", " ")
+            print(f"Processing {team_name}")
+
+            data = requests.get(team_url)
+
+            matches = pd.read_html(data.text, match="Scores & Fixtures")[0]
+            matches["team"] = team_name
+            matches["season"] = year
+
+            all_matches.append(matches)
+
+            time.sleep(10)
+
+        time.sleep(10)
+
+    match_df = pd.concat(all_matches)
+    match_df.to_csv("matches.csv")
